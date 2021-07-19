d8736ad3908c033e6ce1b61acce6fa8983eaa84b
Domryuken <ladonjackson@hotmail.co.uk>
2021-07-19 18:36:25 +0100

    testing modal windows

--- src/controllers/WorkoutsController.tsx
+++ src/controllers/WorkoutsController.tsx
@@ -5,10 +5,18 @@ import {Route} from "react-router-dom"
 
 export const WorkoutsController: React.FC<{workouts: WorkoutModel[]}> = (workouts) => {
 
+  const [modalIsOpen, setIsOpen] = React.useState<boolean>(true);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
-    <div className="App">
-      <WorkoutsView {...workouts}/>
-    </div>
+    <WorkoutsView {...workouts} modalIsOpen={modalIsOpen} openModal={openModal} closeModal={closeModal}/>
   );
 }
 
--- src/views/workoutsPage/WorkoutsView.tsx
+++ src/views/workoutsPage/WorkoutsView.tsx
@@ -1,17 +1,32 @@
 import React from "react";
+import Modal from "react-modal";
 import WorkoutModel from "../../models/WorkoutModel"
+import AddWorkoutView from "../addWorkoutPage/AddWorkoutView"
 import {Workout} from "./Workout"
 import {Link} from "react-router-dom"
+import {addWorkout} from "../../Connector"
 
-
-export const WorkoutsView: React.FC<{workouts: WorkoutModel[]}> = ({workouts}) => {
+export const WorkoutsView: React.FC<{workouts: WorkoutModel[], modalIsOpen: boolean, openModal: () => void, closeModal: () => void}> = ({
+  workouts,
+  modalIsOpen,
+  openModal,
+  closeModal
+}) => {
   return (
+
+    
     <div className="workout-tab-box">
       <h1>Workouts</h1>
       <div className="workout-tab-box-inner">
         <Link to="/add-workout">
           <p>add</p>
         </Link>
+        <Modal
+         isOpen={modalIsOpen}
+         onRequestClose={closeModal}
+         contentLabel="Example Modal">
+          <AddWorkoutView add={addWorkout}/>
+        </Modal>
         {workouts.map( (workout, index) =>
           <Workout
             key={`workout-${index}`}
